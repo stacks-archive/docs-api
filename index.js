6ab8e7bfac535062da1679a31252ba4d02c77be5
Thomas Osmonson <thomas.osmonson@gmail.com>
2019-04-12 17:09:12 -0500

    add headers

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
+const {send} = require('micro')
 const fetch = require('cross-fetch');
 
 const JSON_URL = 'https://docs.blockstack.org/faqs/allfaqs.json'
 
-module.exports = async () => {
-    const response = await fetch(JSON_URL);
-    const json = await response.json();
+module.exports = async (req, res) => {
+  const response = await fetch(JSON_URL);
+  const json = await response.json();
 
-    let categories = {}
-    await Promise.all(json.faqs.map(async (question) => {
-        categories[question.category] = json.faqs.filter(q => q.category === question.category)
-    }))
+  let categories = {}
+  await Promise.all(json.faqs.map(async (question) => {
+    categories[question.category] = json.faqs.filter(q => q.category === question.category)
+  }))
 
-    return categories;
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'origin, content-type');
+  return send(res, 200, categories);
 };
